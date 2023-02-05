6fd2483a9385430e4589209c6d0212a4d6659997
Michelle Lam <michellelamhelps@gmail.com>
2023-02-05 01:54:26 -0500

    SECOND SCREEN WORKS!!!

--- src/mainScreen.py
+++ src/mainScreen.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 import sys
 
 class main_Screen:
@@ -13,8 +12,8 @@ class main_Screen:
         self.size = (self.width, self.height)
         self.resizeable = pygame.RESIZABLE
         self.title = pygame.display.set_caption('Busy Planner')
-        self.screen = pygame.display.set_mode((self.width, self.height), self.resizeable)
-        self.background = pygame.image.load('assets/background.png')
+        # self.screen = pygame.display.set_mode((self.width, self.height), self.resizeable)
+        # self.background = pygame.image.load('assets/background.png')
 
     def mainLoop(self):
         #keep the screen running
@@ -39,6 +38,9 @@ class main_Screen:
 
 
     def startScreen(self):
+        self.screen = pygame.display.set_mode((self.width, self.height), self.resizeable)
+        self.background = pygame.image.load('assets/background.png')
+        
         start_button = pygame.image.load('assets/start_button.png')
         start_size = start_button.get_size()
         rect = start_button.get_rect()
@@ -53,7 +55,8 @@ class main_Screen:
                 if event.type == pygame.MOUSEBUTTONDOWN :
                     if mouse[0] > 478 and mouse[0] < 995 and mouse[1] > 403 and mouse[1] < 545:
                         self.state = "PLANNING SCREEN"
-
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
             self.screen.blit(self.background, (0,0))
             self.screen.blit(start_button, (475,400))
             pygame.display.flip()
@@ -62,10 +65,14 @@ class main_Screen:
 
     def planningScreen(self):
         self.screen = pygame.display.set_mode((self.width, self.height), self.resizeable)
-        # self.screen = pygame.transform.smoothscale(self.background2, self.screen.get_size())
-        self.background = pygame.image.load('assets/background2.png')
+        self.background = pygame.image.load('assets/planning.png')
+        # self.screen = pygame.transform.smoothscale(self.background, self.screen.get_size())
+
 
         while self.state == "PLANNING SCREEN":
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
             self.screen.blit(self.background, (0,0))
             pygame.display.flip()
-        #  pygame.display.update(self.screen)
